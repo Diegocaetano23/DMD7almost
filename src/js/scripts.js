c229a9187f22dbfd1d7de3e28effc1200dd68fbe
Diegocaetano23 <69208564+Diegocaetano23@users.noreply.github.com>
2024-08-06 15:41:45 -0300

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -41,3 +41,91 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 /*fade up fim*/
+
+/* GALLERY SCRIPT */
+document.addEventListener('DOMContentLoaded', () => {
+    const gallery = document.querySelector('.gallery');
+    const items = document.querySelectorAll('.gallery-item');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+
+    let index = 0;
+    const totalItems = items.length;
+    const visibleItems = window.innerWidth >= 600 ? 3 : 1;
+
+    function updateGallery() {
+        const itemWidth = gallery.clientWidth / visibleItems;
+        gallery.style.transform = `translateX(-${index * itemWidth}px)`;
+    }
+
+    function next() {
+        if (index < totalItems - visibleItems) {
+            index++;
+        } else {
+            index = 0;
+        }
+        updateGallery();
+    }
+
+    function prev() {
+        if (index > 0) {
+            index--;
+        } else {
+            index = totalItems - visibleItems;
+        }
+        updateGallery();
+    }
+
+    nextButton.addEventListener('click', next);
+    prevButton.addEventListener('click', prev);
+
+    setInterval(next, 5000); // Adjust the interval time as needed
+
+    window.addEventListener('resize', updateGallery);
+    updateGallery(); // Initial call to set the gallery position
+});
+
+
+
+/* FIM GALLERY*/
+
+
+/*Experience*/
+
+// Função para animar a contagem
+function animateCounter(id, start, end, duration) {
+    let element = document.getElementById(id);
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    
+    let timer = setInterval(() => {
+        current += increment;
+        element.innerText = "+" + current; // Adiciona o símbolo "+" antes do número
+        if (current === end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+// Função para iniciar a animação quando o elemento entrar na viewport
+function startAnimation(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            animateCounter('experience-years', 0, 10, 2000);
+            animateCounter('experience-projects', 0, 250, 2000);
+            observer.disconnect(); // Para de observar depois de iniciar a animação
+        }
+    });
+}
+
+// Configura o Intersection Observer
+let observer = new IntersectionObserver(startAnimation, {
+    threshold: 0.5 // Inicia a animação quando 50% da div estiver visível
+});
+
+// Observa a div com a classe 'experience'
+observer.observe(document.querySelector('.experience'));
+
+/* EXPERIENCE FIM*/
